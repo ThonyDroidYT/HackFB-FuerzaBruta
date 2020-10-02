81c73e328270c8ec4afa5330a2a04b62e6e16f74
ThonyDroidYT <69366306+ThonyDroidYT@users.noreply.github.com>
2020-10-01 19:16:56 -0500

    Update brute.py

--- brute.py
+++ brute.py
@@ -62,7 +62,7 @@ cover()
 
 email = str(raw_input(GL+" â€¢Introduzca la ID del objetivo\033[33;1m: "))
 
-passwordlist = str(raw_input(WW+" â€¢Ingrese el archivo de Contraseñas\033[95m[ pass.txt, pass1.txt, pass2.txt, pass3.txt ] \033[92;1m: "))
+passwordlist = str(raw_input(WW+" â€¢Ingrese el archivo de Contraseñas\033[95m[ pass.txt, pass1.txt, pass2.txt, pass3.txt pass4.txt] \033[92;1m: "))
 
 
 #login = 'https://m.facebook.com/login/?ref=dbl&fl&refid=8'
